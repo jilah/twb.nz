d0c3d2daf0670a2a85365e4b05cb8906c439909e
Jilah <111175989+jilah@users.noreply.github.com>
2022-09-12 23:54:28 +0700

    Update and rename apk.js to modals.js

---
+++ js/modals.js
@@ -0,0 +1,11 @@
+(function($) {
+            "use strict";
+                $(document).ready(function() {
+                        $('.button-3').on('click', function() {
+                        $('body').addClass("modal-open");
+                    });
+                    $('.close-modal').on('click', function() {
+                        $('body').removeClass("modal-open");
+                    });
+                });
+        }(jQuery));
