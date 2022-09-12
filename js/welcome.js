cbf62a55281c5a32559b1310c8cea962c6d8abc4
Jilah <111175989+jilah@users.noreply.github.com>
2022-09-12 10:28:12 +0700

    Create welcome.js

---
+++ js/welcome.js
@@ -0,0 +1,14 @@
+// Add your code here
+
+const modal = document.getElementById("modal");
+const btnSkip = document.getElementById("modal-skip");
+
+window.onload = (event) => {
+  setTimeout(() => modal.classList.add("modal-visible"), 1000);
+};
+
+//tap outside overlay or on cancel button to close window
+
+btnSkip.addEventListener("click", () => {
+  modal.classList.remove("modal-visible");
+});
