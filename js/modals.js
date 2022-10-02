246e80b0e2644bb39080e21077398ab40ed076d3
Jilah <111175989+jilah@users.noreply.github.com>
2022-10-02 16:17:12 +0700

    Update modals.js

--- js/modals.js
+++ js/modals.js
@@ -1,7 +1,7 @@
 (function($) {
             "use strict";
                 $(document).ready(function() {
-                        $('.button-3').on('click', function() {
+                        $('.press press-black press-round press-ghost').on('click', function() {
                         $('body').addClass("modal-open");
                     });
                     $('.close-modal').on('click', function() {
